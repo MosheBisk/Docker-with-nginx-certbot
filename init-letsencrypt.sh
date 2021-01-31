e24babe5bd73023dcd3a43b88a2dacf8e70f081a
MA Bisk <moshebisk@gmail.com>
2021-01-31 18:44:27 +0200

    remove port from paths

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(jenkins.madan.ravtech.co.il:8080)
+domains=(jenkins.madan.ravtech.co.il)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="moshe.bisk@ravtech.co.il" # Adding a valid address is strongly recommended
